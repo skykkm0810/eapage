ccd6139a387cbfc4eaa6d9d4c66c0ed4fe2729c0
skykkm0810 <skykkm0810@naver.com>
2021-01-28 03:03:21 +0900

    mainComplete

--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
     var dot = document.getElementsByClassName('dot');
     var dotWrap = document.getElementsByClassName('dotWrap')[0] as HTMLElement;
@@ -16,5 +15,43 @@ export class MainComponent implements OnInit {
     console.log(wrapWidth)
     dotWrap.style.left = 'calc(50% - '+(wrapWidth/2)+'px)';
   }
+  autoSlide = setInterval(()=>{this.slideLeft()},5000)
+  curIndex = 0;
 
+  slideLeft(){
+    this.curIndex ++;
+    var slideBody = document.getElementsByClassName('slideBody')[0] as HTMLElement;
+    var slideList = slideBody.getElementsByTagName('li');
+    var dot = document.getElementsByClassName('dot');
+    if(this.curIndex == slideList.length){
+      this.curIndex = 0;
+      slideBody.style.marginLeft = '0';
+      for(var i=0; i<dot.length;i++){
+        dot[i].classList.remove('show')
+      }
+      dot[0].classList.add('show')
+    }
+    else{
+      slideBody.style.transition = 'left , 0.5s';
+      slideBody.style.marginLeft = -100*this.curIndex + '%';
+      for(var i=0; i<dot.length;i++){
+        dot[i].classList.remove('show')
+      }
+      dot[this.curIndex].classList.add('show')
+    }
+  }
+  dotClick(e:Event){
+    var thisDot = e.target as HTMLElement;
+    var idx:any ;
+    var dot = document.getElementsByClassName('dot');
+    for(var i=0; i<dot.length;i++){
+      if( thisDot == dot[i]){
+        idx = i;
+      }
+    }
+    this.curIndex = idx - 1;
+    clearInterval(this.autoSlide)
+    this.slideLeft();
+    this.autoSlide = setInterval(()=>{this.slideLeft()},5000)
+  }
 }
